1c31399ceed041e4ef6749e0c65ecc55432e6614
Sania-Ahmed <tasminsania77@gmail.com>
2024-08-27 19:29:04 -0700

    done primarily

--- components/complement.js
+++ components/complement.js
@@ -1,10 +1,22 @@
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faArrowUpLong } from '@fortawesome/free-solid-svg-icons';
+import { getRandomQuote } from "../lib/randomQuote";
+import Swal from 'sweetalert2';
 
 
 const Complement = () => {
+  const getQuote = async () => {
+    const quote = await getRandomQuote();
+
+    Swal.fire({
+      title: 'Quote',
+      text: `${quote.q}\n - ${quote.a}`,
+      confirmButtonText: 'Cool'
+    })
+  }
   return(
-  <div className="mb-3 w-full flex justify-center">
+  <div className="mb-3 w-full flex justify-center" >
+    <button onClick={getQuote}>
     <div className='wavy-bg  drop-shadow-lg rounded-xl md:w-80 w-auto p-2 md:p-4 flex justify-evenly items-center'>
         <div className='text-center md:text-lg font-semibold text-cyan-950'>
             <p>Get a complement !</p>
@@ -13,6 +25,8 @@ const Complement = () => {
           <FontAwesomeIcon className='w-full h-full text-cyan-950  hover:text-slate-100  ' icon={faArrowUpLong} />
         </div>
     </div>
+    </button>
+  
   </div>
   )
 };
